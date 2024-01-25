ae6a63004abed7b6c08b583ebfc3d32b5480e7c4
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2024-01-25 20:19:21 -0300

    docs: document the classes and functions. also remove a unused class 'MusicPlayer'

--- graphs_musical_scale/musical_scales.py
+++ graphs_musical_scale/musical_scales.py
@@ -3,9 +3,25 @@ import random
 
 
 class Music:
+    """A class representing music composition and playback.
+
+    Attributes:
+        name (str): The name of the music composition.
+        tone (str): The tone of the music composition.
+        interval_type (str): The interval type of the music composition.
+        notes (list): The list of notes in the music composition.
+        music (list): The generated melody of the music composition.
+    """
 
     def _make_melody(self, notes_to_use, size: int = 12):
-        """ Randomly select notes to create a melody.
+        """Randomly select notes to create a melody.
+
+        Args:
+            notes_to_use (dict): A dictionary containing the available notes.
+            size (int, optional): The size of the melody. Defaults to 12.
+
+        Returns:
+            list: The generated melody.
         """
         melody = []
         for _ in range(size):
@@ -16,17 +32,32 @@ class Music:
         return melody
 
     def compose_music(self, scale: str, tone: str, interval_type: str):
+        """Compose music based on the given scale, tone, and interval type.
+
+        Args:
+            scale (str): The musical scale to use.
+            tone (str): The tone of the composition.
+            interval_type (str): The interval type of the composition.
+
+        Returns:
+            list: The generated notes of the composition.
+        """
         self.name = f'Sonata in {tone} {scale}'
         self.tone = tone
         self.interval_type = interval_type
         self.notes = MusicalScale().generate(scale, tone, interval_type)
         self.music = self._make_melody(self.notes)
 
-        # print(self.__dict__)
         return self.notes
 
     def _get_frequency(self, note):
-        """ Return the frequency of a given note.
+        """Return the frequency of a given note.
+
+        Args:
+            note (str): The note for which to retrieve the frequency.
+
+        Returns:
+            int: The frequency of the note.
         """
         note_frequencies = {
             'C': 261,
@@ -49,27 +80,27 @@ class Music:
         }
         return note_frequencies[note]
 
-    def play_music(self,):
+    def play_music(self):
+        """Play the composed music."""
         print(f'Playing {self.name} in {self.tone} tone, in {self.interval_type} scale.')
         for note in self.music:
-            # print(note, end=' ')
             frequency = self._get_frequency(note)  # Gets note frequency
-            duration = 500  # Note duration in miliseconds
+            duration = 500  # Note duration in milliseconds
             winsound.Beep(frequency, duration)  # play the sound
 
 
 class MusicalScale:
     """
-        Musical Scale class to generate scales and the graph of cromatic scale: minor and major,
-        ascending and descending.
+    Musical Scale class to generate scales and the graph of cromatic scale: minor and major,
+    ascending and descending.
     """
 
     def __init__(self) -> None:
         # distance (intervals) between each degree,
         # Starts in the 2nd degree, finish in 7th, tonic is 0
         self.major_scale_intervals = [0, 2, 2, 1, 2, 2, 2, ]
-        # Starts in the 2nd degree, finish in 7th, tonic is 0
         self.minor_scale_intervals = [0, 2, 1, 2, 2, 2, 1, ]
+
         # Starts in the 2nd degree, finish in 4th, tonic is 0
         self.pentatonica_scale_intervals = [0, 2, 2, 3, 2, ]
 
@@ -79,6 +110,18 @@ class MusicalScale:
                                           'A', 'G', 'Gb', 'F', 'E', 'Eb', 'D', 'Db', 'C']
 
     def _generate_scale(self, tonic: str, intervals: list, chromatic_scale: list):
+        """
+        Generates a musical scale based on the given tonic, intervals, and chromatic scale.
+
+        Args:
+            tonic (str): The tonic note of the scale.
+            intervals (list): The intervals between each degree of the scale.
+            chromatic_scale (list): The chromatic scale used as the base.
+
+        Returns:
+            dict: A dictionary representing the generated scale, where the keys are the notes
+            and the values are the intervals.
+        """
         scale = {}  # if we want to add the tonic note, we can use this line: scale[tonic] = 0
         current_note = tonic
         for interval in intervals:
@@ -88,6 +131,16 @@ class MusicalScale:
         return scale
 
     def _next_note(self, current_note, chromatic_scale: list):
+        """
+        Returns the next note in the chromatic scale based on the current note.
+
+        Args:
+            current_note (str): The current note.
+            chromatic_scale (list): The chromatic scale.
+
+        Returns:
+            str: The next note in the chromatic scale.
+        """
         notes = chromatic_scale
         current_index = notes.index(current_note)
         next_index = (current_index + 1) % len(notes)
@@ -99,6 +152,21 @@ class MusicalScale:
         tonic: str = 'C',
         interval_type: str = 'ascending'
     ):
+        """
+        Generates a musical scale based on the given parameters.
+
+        Args:
+            scale (str, optional): The type of scale to generate. Defaults to 'major'.
+            tonic (str, optional): The tonic note of the scale. Defaults to 'C'.
+            interval_type (str, optional): The type of interval to use. Defaults to 'ascending'.
+
+        Returns:
+            dict: A dictionary representing the generated scale, where the keys are the notes
+            and the values are the intervals.
+
+        Raises:
+            ValueError: If the scale is not 'major' or 'minor'.
+        """
         cromatic_scale = (self.cromatic_scale_ascending
                           if interval_type == 'ascending'
                           else self.cromatic_scale_descending)
@@ -120,6 +188,17 @@ class MusicalScale:
         scale: str = 'major',
         interval_type: str = 'ascending'
     ):
+        """
+        Generates a graph of complete scales based on the given parameters.
+
+        Args:
+            scale (str, optional): The type of scale to generate. Defaults to 'major'.
+            interval_type (str, optional): The type of interval to use. Defaults to 'ascending'.
+
+        Returns:
+            dict: A dictionary representing the complete scales graph, where the keys are the notes
+            and the values are the scales.
+        """
         graph = {}
         cromatic_scale = (self.cromatic_scale_ascending
                           if interval_type == 'ascending'
@@ -129,31 +208,3 @@ class MusicalScale:
             graph[note] = self.generate(scale, note, interval_type)
 
         return graph
-
-
-class MusicPlayer():
-    def _get_frequency(self, note):
-        # Mapeia as notas para as frequências correspondentes
-        note_frequencies = {
-            'C': 261,
-            'Db': 277,
-            'D': 293,
-            'Eb': 311,
-            'E': 329,
-            'F': 349,
-            'Gb': 369,
-            'G': 392,
-            'Ab': 415,
-            'A': 440,
-            'Bb': 466,
-            'B': 493
-        }
-        return note_frequencies[note]
-
-    def play_music(self, music: Music):
-        print(f'Playing "{music.name}" in {music.tone} scale.')
-        for note in music.music:
-            frequency = self._get_frequency(note)  # Obtenha a frequência da nota
-            duration = 1000  # Duração da nota em milissegundos
-            winsound.Beep(frequency, duration)  # Reproduz o som
-            print(note, end=' ')
